e7fd5036c75bf8dc5b692af219b7dc9fc29f839d
Lucas Aguilar <lucasaguilar835@gmail.com>
2022-05-10 10:29:46 -0300

    Usuário vê detalhes do galpão e volta para a tela inicial

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -24,12 +24,12 @@ describe 'Usuario visita tela inicial' do
     expect(page).to have_content('Rio')
     expect(page).to have_content('Código: SDU')
     expect(page).to have_content('Cidade: Rio de Janeiro')
-    expect(page).to have_content('60000 M²')
+    expect(page).to have_content('60000 m²')
 
     expect(page).to have_content('Maceio')
     expect(page).to have_content('Código: MCZ')
     expect(page).to have_content('Cidade: Maceio')
-    expect(page).to have_content('50000 M²')
+    expect(page).to have_content('50000 m²')
   end
 
   it 'e não existem galpões cadastrados' do
