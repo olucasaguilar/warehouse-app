30e276302ebbe818b40d33767042d9883a8a76f1
Lucas Aguilar <lucasaguilar835@gmail.com>
2022-05-30 14:22:08 -0300

    Adiciona model product_models e tela index do mesmo

---
+++ spec/system/user_view_product_models_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+describe 'Usuário vê modelos de produtos' do
+  it 'a partir do menu' do
+    # Arrange
+
+    # Act
+    visit root_path
+    within('nav') do
+      click_on 'Modelos de Produtos'
+    end
+    # Assert
+    expect(current_path).to eq product_models_path 
+  end
+
+  it 'com sucesso' do
+    # Arrange
+    supplier = Supplier.create!(brand_name: 'Samsung', corporate_name: 'Samsung Eletronicos LTDA',
+                                registration_number: '1234567891234', full_address: 'Av Nacoes,1000',
+                                city: 'São Paulo', state: 'SP', email: 'sac@samsung.com')
+    ProductModel.create!(name: 'TV 32', weight: 8000, width: 70, height: 45, depth: 10,
+                         sku: 'TV32-SAMSU-XPTO90', supplier: supplier)
+    ProductModel.create!(name: 'SoundBar 7.1 Surround', weight: 3000, width: 80, height: 15, 
+                         depth: 20, sku: 'SOU71-SAMSU-NOIZ77', supplier: supplier)
+    # Act
+    visit root_path
+    within('nav') do
+      click_on 'Modelos de Produtos'
+    end
+    # Assert
+    expect(page).to have_content 'TV 32'
+    expect(page).to have_content 'TV32-SAMSU-XPTO90'
+    expect(page).to have_content 'Samsung'
+    expect(page).to have_content 'SoundBar 7.1 Surround'
+    expect(page).to have_content 'TV 32'
+    expect(page).to have_content 'SOU71-SAMSU-NOIZ77'
+  end
+
+  it 'e não existem produtos cadastrados' do
+    # Arrange
+
+    # Act
+    visit root_path
+    click_on 'Modelos de Produtos'
+
+    # Assert
+    expect(page).to have_content 'Nenhum modelo de produto cadastrado.'
+  end
+end
