a340ce20deed781589d7bde992396e684f49e80a
Lucas Aguilar <lucasaguilar835@gmail.com>
2022-05-31 00:05:13 -0300

    Prepara gem Devise para autenticação e habilita apenas a função de login por enquanto

---
+++ spec/system/user_sign_in_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'Usuário se autentica' do
+  it 'com sucesso' do
+    # Arrange
+    User.create!(email: 'lucas@gmail.com', password: 'password')
+    # Act
+    visit root_path
+    click_on 'Entrar'
+    within('form') do
+      fill_in 'E-mail', with: 'lucas@gmail.com'
+      fill_in 'Senha', with: 'password'
+      click_on 'Entrar'
+    end
+
+    # Assert
+    expect(page).to have_content 'Login efetuado com sucesso.'
+    within('nav') do
+      expect(page).not_to have_link 'Entrar'
+      expect(page).to have_link 'Sair'
+      expect(page).to have_content 'lucas@gmail.com'
+    end
+  end
+end
