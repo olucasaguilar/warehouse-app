de981c311029ecceccecfbc8c7f475296d97257e
Lucas Aguilar <lucasaguilar835@gmail.com>
2022-05-31 00:26:50 -0300

    Adiciona função para user conseguir fazer logout

--- spec/system/user_sign_in_spec.rb
+++ spec/system/user_sign_in_spec.rb
@@ -17,8 +17,27 @@ describe 'Usuário se autentica' do
     expect(page).to have_content 'Login efetuado com sucesso.'
     within('nav') do
       expect(page).not_to have_link 'Entrar'
-      expect(page).to have_link 'Sair'
+      expect(page).to have_button 'Sair'
       expect(page).to have_content 'lucas@gmail.com'
     end
+  end
+
+  it 'e faz logout' do
+    # Arrange
+    User.create!(email: 'lucas@gmail.com', password: 'password')
+    # Act
+    visit root_path
+    click_on 'Entrar'
+    within('form') do
+      fill_in 'E-mail', with: 'lucas@gmail.com'
+      fill_in 'Senha', with: 'password'
+      click_on 'Entrar'
+    end
+    click_on 'Sair'
+    # Assert
+    expect(page).to have_content 'Logout efetuado com sucesso.'
+    expect(page).to have_link 'Entrar'
+    expect(page).not_to have_button 'Sair'
+    expect(page).not_to have_content 'lucas@gmail.com'
   end
 end
